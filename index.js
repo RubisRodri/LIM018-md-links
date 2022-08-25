7505347a63f1b76ec3b53cebeba1f8ff74206a4e
RubisRodri <rubisrodri@gmail.com>
2022-08-24 21:36:35 -0500

    incorporando funciones para leer archivos

--- index.js
+++ index.js
@@ -1,62 +1,49 @@
-const md = require('./md-method')
+const { convertPathToAbsolut, existPath, extPath, readDoc, isDirectory, readLinks } = require("./md-method.js");
+//const fs = require("fs");
+//const { extname } = require("path");
 //const path = require('path');
-//import { existPath, convertPathToAbsolut } from './md-method.js'
 
-
-let rutacompleta ='C:\proyecto 4\LIM018-md-links'
-let firname = "\rubis\videos";
-
-
-
-
-
-console.log(path.isAbsolute(__dirname))
-console.log(__dirname)
-
-//console.log(path.extname(rutacompleta))
-
-//console.log(md.getMdFiles(rutacompleta));
-
-// LEER EL ARCHIVO MARKDOWN
-//const readFile = (file) => fs.readFileSync(file, 'UTF-8');
-//const fileContent = (pathAbsolute) => readFile(pathAbsolute);
-
-
-
-
-//console.log(path.isAbsolute('C:\proyecto 4\LIM018-md-links')); false
-//console.log(method.convertPathToAbsolut('C:\proyecto 4\LIM018-md-links\isPath'));  es absoluta C:proyecto 4LIM018-md-linksisPath
-//console.log(md.existPath('/proyecto 4/LIM018-md-links'))
-//console.log(method.existPath('C:\proyecto 4\LIM018-md-links')) la ruta no existe undefined
-//console.log(path.isAbsolute('C:/proyecto 4/LIM018-md-links'))   
-//console.log(path.isAbsolute('C:\proyecto 4\LIM018-md-links'))  false
+const ejemplo ='C:/proyecto 4/LIM018-md-links/prueba'; // funciona como ejemplo de directorio
+const ruta = 'C:/proyecto 4/LIM018-md-links';   //ejemplo de no contiene link
+const ejemplo2 = 'C:/proyecto 4/LIM018-md-links/prueba2.md';  // este funciona como ejemplo
 //console.log(__dirname)
 
-//console.log(path.normalize('C:\proyecto 4\LIM018-md-links'))  C:proyecto 4LIM018-md-links
-//console.log(path.isAbsolute('C:proyecto 4LIM018-md-links'))  false
-//console.log(path.join('C:\proyecto 4\LIM018-md-links'))
-//console.log(path.normalize('C:proyecto 4LIM018-md-links'))
+ const mdLinks = ( ispath, Option = {validate:false}) =>new Promise((resolve, reject) => {
+    const absolutePath = convertPathToAbsolut(ispath);
+    const validePath = existPath(absolutePath);
+    let arrayLinks;
+    if(validePath === false){
+      //reject('la ruta no es valida');
+      console.log('ruta no valida')
+    } else if(validePath){
+       console.log('ruta valida y es un directorio')
+       const rutaDir = isDirectory(absolutePath); // devuelve un booleano
+
+     } if (!isDirectory(absolutePath)){
+        console.log('es un documento')
+        let arrayFile = readDoc(absolutePath);
+        let resultado = readLinks(arrayFile, absolutePath); // averiguar el desempaque de la promesa aqui
+        resolve (resultado)
+       }
+    
+
+ });
+
+
+
+ mdLinks(ejemplo2).then( (res) =>{
+  console.log(res)
+ }).catch
+
+
+
+
+  // console.log(readDoc(ejemplo2))
 
 
 
 
 
-/*const mdLinks = (path, options = { validate: false }) => new Promise((resolve, reject) => {
-  // invoca funcion converterPath
-  const absolutePath = converterPath(path);
-
-  // invoca funcion mdsArraysValidation con el resultado de pathValidation CALLBACK
-  mdsArraysValidation(options, pathValidation(absolutePath))
-    .then((result) => {
-      resolve(result);
-    })
-    .catch((error) => {
-      reject(error);
-    });
-});
-*/
-// se exporta funcion mdLinks
-//module.exports = mdLinks;
 
 
 
@@ -69,4 +56,50 @@ console.log(__dirname)
 
 
 
+
+
+
+
+
+
+
+
+
+
+const ejemplo3 =`[Markdown](https://es.wikipedia.org/wiki/Markdown) es un lenguaje de marcado
+ligero muy popular entre developers. Es usado en muchísimas plataformas que
+manejan texto plano (GitHub, foros, blogs, ...) y es muy común
+encontrar varios archivos en ese formato en cualquier tipo de repositorio
+(empezando por el tradicional README.md).
+
+Estos archivos Markdown normalmente contienen _links_ (vínculos/ligas) que
+muchas veces están rotos o ya no son válidos y eso perjudica mucho el valor de
+la información que se quiere compartir.
+
+Dentro de una comunidad de código abierto, nos han propuesto crear una
+herramienta usando [Node.js](https://nodejs.org/), que lea y analice archivos
+en formato Markdown, para verificar los links que contengan y reportar
+algunas estadísticas.
+
+![md-links](https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg)
+
+## 2. Resumen del proyecto`;
+
+//console.log(readLinks( `[Markdown](https://es.wikipedia.org/wiki/Markdown) es un lenguaje de marcado              
+//ligero muy popular entre developers. Es usado en muchísimas plataformas que                                     
+//manejan texto plano (GitHub, foros, blogs, ...) y es muy común usando [Node.js](https://nodejs.org/)` , ruta))  
+
+//const links = ejemplo3.match(/\[(.*?)\]\(.*?\)/gm); esto funciona
+//console.log(links)                                  me devuelve los link en un array
+
+
+//console.log(readLinks( './.readme.md', ruta))
+
+
+
+
+
+//mdLinks(ejemplo2)
+
+module.exports = mdLinks;
 
--- md-method.js
+++ md-method.js
@@ -1,17 +1,16 @@
-const fs = require('fs');
-const path = require('path');
-
+const fs = require("fs");
+const path = require("path");
+const { extname } = require("path");
 
 //funcion para convertir una ruta de relativa a absoluta.
 
 const convertPathToAbsolut = (isPath) => {
   if (path.isAbsolute(isPath)) {
-    console.log(' es absoluta')
+    console.log(" es absoluta");
     return isPath;
-
   } else {
-      console.log('convirtiendo relativa a absoluta')
-      return path.resolve(isPath).normalize();
+    console.log("La ruta no es absoluta");
+    return path.resolve(isPath).normalize();
   }
 };
 
@@ -19,35 +18,97 @@ const convertPathToAbsolut = (isPath) => {
 
 const existPath = (isPath) => fs.existsSync(isPath);
 
+// funcion para verificar la extencion del documento
+const extPath = (isPath) => {
+  const extName = path.extname(isPath);
+  console.log(extName);
 
-// funcion para obtener array de rutas. md
-
-const getMdFiles = (allMdFiles, isPath) => {
-  const isDirRes = fs.statSync(isPath).isDirectory(); //Verifica si la ruta es directorio (true or False)
-  if (isDirRes) { //Si es Directorio
-      const allDirFiles = fs.readdirSync(isPath); //Array con el contenido del directorio
-      allDirFiles.forEach((file) => {
-          const absolutPath = path.join(isPath, file);
-          getMdFiles(allMdFiles, absolutPath)
-      })
+  if (extName === ".md") {
+    return true;
   } else {
-      const isMdFiles = path.extname(isPath); //Extencion del archivo
-      if (isMdFiles === '.md') { //Si es archivo .md
-          allMdFiles.push(isPath);
-      }
+    return false;
   }
-  return allMdFiles;  //Retorna array de Path .md
+};
+
+// funcion para verificar si es un directorio.
+
+const isDirectory = (isPath) => {
+  const isDirectory = fs.statSync(isPath).isDirectory();
+  return isDirectory;
 };
 
 
+// funcion para leer archivo
+const readDoc = (isPath) => fs.readFileSync(isPath, "UTF-8");
 
+// LEER EL ARCHIVO MARKDOWN
+//const readFile = (file) => fs.readFileSync(file, 'UTF-8');
+//const fileContent = (pathAbsolute) => readFile(pathAbsolute);
 
+// EXTRAER LOS LINKS DEL ARCHIVO MARKDOWN
 
-module.exports = { convertPathToAbsolut, existPath, getMdFiles};
+const readLinks = (content, isPath) => new Promise ((resolve) => {
+  const regExp1 = new RegExp (/\[(.*?)\]\(.*?\)/gm);//link
+  const regExp2 = new RegExp (/\[[\w\s\d.()]+\]/);//texto
+  const regExp3 = new RegExp (/\(((?:\/|https?:\/\/)[\w\d./?=#&_%~,.:-]+)\)/mg);//ruta
+  const fileContent = content;//lee el archivo
+  const links = fileContent.match(regExp1);/* extraigo los links que coincidan con mi expresion regular
+      match() se usa para obtener todas las ocurrencias de una expresión regular dentro de una cadena.*/
+      //console.log(links)
+  //console.log('arrayLinks: ', arrayLinks);
+  let arrayLinks;
+  if (links) {
+      arrayLinks = links.map((myLinks) => {
 
+          const myhrefArray = myLinks.match(regExp3) ?? []          
+          const myhref = myhrefArray.join().slice(1, -1);//URL ()
 
+          const mytextArray = myLinks.match(regExp2) ?? []
+          const mytext = mytextArray.join().slice(1, -1);//Texto []
+          return {
+              href: myhref,
+              text: mytext,
+              fileName: isPath,//Ruta del archivo donde se encontró el link.
+          };
+      });
+      resolve (arrayLinks)        
+  } else if (links === null){
+      resolve ([]);
+      console.log('el archivo no contiene links')
+  }
+});
 
+    /*if (readDoc !== null) {
+      const linksArray = readDoc.match(regxLink); // revisa content archivo para capturar links
+    }
+    if (linksArray === null) {
+      // si no hay links en archivo retorna []
+      resolve([]);
+    }
 
+    const turnedLinksArray = linksArray.map((myLinks) => {
+      // transforma arr links y entrega objt
+      const myhref = myLinks.match(regxUrl).join().slice(1, -1); // URL encontradas
+      const mytext = myLinks.match(regxText).join().slice(1, -1); // texto que hace ref a URL
+      return {
+        href: myhref,
+        text: mytext.substring(0, 50),
+        fileName: path.basename(pathMd), // ruta de URL
+      };
+      resolve(turnedLinksArray);
+    });
+      reject(error);
+     console.log(error)
+  });
+  */
 
+//const fileContent = (pathAbsolute) => readFile(pathAbsolute);
 
-
+module.exports = {
+  convertPathToAbsolut,
+  existPath,
+  extPath,
+  readLinks,
+  readDoc,
+  isDirectory
+};
